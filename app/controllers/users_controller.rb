55892a5d85c9241034067df574aa12cfe57c5990
Michael Taylor <mjt145@gmail.com>
2013-09-12 20:47:48 +0100

    add user show action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  
+  # GET /pins/1
+  def show
+    @user = User.find(params[:id])
+    @pins = @user.pins.page(params[:page]).per_page(20)
+  end
+
+end
